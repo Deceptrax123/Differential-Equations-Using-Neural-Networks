d0464ecd0967c1863dade18b2cd46c02aed4fae7
Deceptrax123 <srinitishsri@gmail.com>
2024-01-14 04:17:10 +0530

    train ode model and results

---
+++ neural_network/predict.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from model import DiffEquationModel
+
+
+def plot_y():
+    t = torch.tensor(np.linspace(0, 5, 200000)[:, None], dtype=torch.float32)
+    t = t.view(t.size(0), 1)
+
+    model = DiffEquationModel()
+    model.load_state_dict(torch.load("neural_network/weights.pth"))
+
+    y_t = t*model(t)
+
+    plt.plot(t.detach().numpy(), y_t.detach().numpy())
+    plt.xlabel("t")
+    plt.ylabel("y(t)")
+    plt.title("Solution using Neural Network")
+    plt.show()
+
+
+plot_y()
--- neural_network/train.py
+++ neural_network/train.py
@@ -1,59 +1,71 @@
-import torch 
-from torch import nn 
-import numpy as np 
+import torch
+from torch import nn
+import numpy as np
 from model import DiffEquationModel
 from initialize import init_weights
 from torch.autograd import grad
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-def analytic_derivative(t,y):
-    return y+(-(1/2)*((torch.exp(t/2)*torch.sin(5*t))))+(5*torch.exp(t/2)*torch.cos(5*t))
+torch.manual_seed(32)
+
+
+def analytic_derivative(t, y):
+    return torch.exp(-t/5)*torch.cos(t)-y/5
+
 
 def network_derivative(t):
-    t.requires_grad=True
-    z=model(t).sum()
-    
-    grad_f=grad(z,t)[0]
+    t.requires_grad = True
+    z = model(t).sum()
+
+    grad_f = grad(z, t)[0]
 
     return grad_f
 
+
 def nn_output(t):
-    A=0
+    A = 0
     return t*model(t)+A
 
-def train():
-    #Set initial conditions
-    t=torch.tensor(np.linspace(0,5,200)[:,None],dtype=torch.float32)
-    t=t.view(t.size(0),1)
 
+def train():
+    # Set initial conditions
+    t = torch.tensor(np.linspace(0, 5, 100)[:, None], dtype=torch.float32)
+    t = t.view(t.size(0), 1)
+
+    sl = 0
     model.train(True)
-    for epoch in range(epochs):        
-        network_deri=network_derivative(t)*t+model(t)
-        analytic_deri=analytic_derivative(t,nn_output(t))
+    for epoch in range(epochs):
+        network_deri = network_derivative(t)*t+model(t)
+        analytic_deri = analytic_derivative(t, nn_output(t))
 
         optimizer.zero_grad()
-        loss=objective(network_deri,analytic_deri)
-        
+        loss = objective(network_deri, analytic_deri)
+
         loss.backward()
         optimizer.step()
 
-        print("Epoch: ",epoch)
-        print("Loss: ",loss.item())
-    
-    torch.save(model.state_dict(),"neural_network/weights.pth")
+        print("Epoch: ", epoch)
+        print("Loss: ", loss.item())
 
-if __name__=='__main__':
-    
-    #Get the model
-    model=DiffEquationModel()
+        if epoch == 0:
+            sl = loss.item()
 
-    init_weights(model)
-    optimizer=torch.optim.Adam(params=model.parameters(),lr=0.001,betas=(0.9,0.999))
+        if loss.item() < sl:
+            sl = loss.item()
+            torch.save(model.state_dict(), "neural_network/weights.pth")
 
-    #Losses
-    objective=nn.MSELoss()
 
-    #Hyperparameters
-    epochs=5000000
+if __name__ == '__main__':
+
+    # Get the model
+    model = DiffEquationModel()
+
+    optimizer = torch.optim.Adam(
+        params=model.parameters(), lr=0.001, betas=(0.9, 0.999))
+    # Losses
+    objective = nn.MSELoss()
+
+    # Hyperparameters
+    epochs = 50000
 
     train()
