fe4489a80de455d0879ba13ecb0462ad3a326813
Deceptrax123 <srinitishsri@gmail.com>
2024-01-14 02:39:17 +0530

    fixes

--- neural_network/initialize.py
+++ neural_network/initialize.py
@@ -6,6 +6,6 @@ def init_weights(model):
     for module in model.modules():
         if isinstance(module,(nn.Linear)):
             if module.bias.data is not None:
-                model.bias.data.zero_()
+                module.bias.data.zero_()
             else:
                 nn.init.kaiming_normal_(module.weight.data,mode='fan_in')
